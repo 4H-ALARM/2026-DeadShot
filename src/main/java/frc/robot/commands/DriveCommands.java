a2ee7eaea47ebb51f775acb256a9a5452e4db1a8
ALARM <alarmrobotixs2079@gmail.com>
2026-04-15 14:35:43 -0400

    tuned some numbers

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -31,7 +31,7 @@ import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
 public class DriveCommands {
-  private static final double DEADBAND = 0.1;
+  private static final double DEADBAND = 0.02;
   private static final double ANGLE_KP = 10;
   private static final double ANGLE_KD = 0.2;
   private static final double ANGLE_MAX_VELOCITY = 16;
